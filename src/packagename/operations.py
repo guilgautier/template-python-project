b3f21d176a1a2039c0a86ed09ef227095162c665
Guillaume Gautier <guillaume.gga@gmail.com>
2021-08-23 14:49:00 +0200

    add source code of packagename

---
+++ src/packagename/operations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+
+def addition(x, y):
+    r"""Compute the addition
+
+    .. math::
+        :label: eq:my-addition
+
+        x + y
+
+    :param x: first arguments
+    :type x: Number
+    :param y: second argument
+    :type y: Number
+    :return: :math:`x + y`
+    :rtype: Number
+    """
+    return x + y
