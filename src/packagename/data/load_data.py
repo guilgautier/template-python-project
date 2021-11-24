d6ae3e07300a64ada25a56284049107e04fd48f6
Guillaume Gautier <guillaume.gga@gmail.com>
2021-11-24 09:46:41 +0100

    data: create data subpackage

---
+++ src/packagename/data/load_data.py
@@ -0,0 +1,27 @@
+import pickle
+from importlib import resources
+
+import pandas
+
+
+def load_mock_csv():
+    return _load_csv("mock.csv")
+
+
+def load_mock_pkl():
+    return _load_pkl("mock.pkl")
+
+
+def _load_csv(file_name):
+    with resources.path("packagename.data", file_name) as path:
+        df = pandas.read_csv(path)
+    return df
+
+
+def _load_pkl(file_name):
+    data = resources.read_binary("packagename.data", file_name)
+    dict_data = pickle.loads(data)
+    # from_dict requires to convert single float values to array_like
+    dict_data = {k: [v] for k, v in dict_data.items()}
+    df = pandas.DataFrame.from_dict(dict_data)
+    return df
